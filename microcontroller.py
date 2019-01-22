d54ed37d2cf055ceaa716542888314efb1553fd0
ApotropaicTroper <ApotropaicTroper@users.noreply.github.com>
2019-01-22 11:15:53 -0500

    Update microcontroller.py

--- microcontroller.py
+++ microcontroller.py
@@ -1,6 +1,3 @@
-Microcontroller code
-
-
 '''
 parts to control:
 	cooling fan
@@ -14,18 +11,13 @@ Use case:
 - Toaster lowers food into position
 - Toaster cooks food using provided instructions
 - Toaster raises food out of position
-
 One thread for each slot. Two slots, so two threads
 A separate thread should listen for instructions from user. Write to data structure which the main code reads from? Its writing takes priority over main program's reading
-
-
 Instruction validity check:
 	Is indicated timer value positive?
 	  N: report entry error
 	Is indicated temperature value greater than some threshold?
 	  N: report low temperature
-
-
 Cook food:
 	loop until timer is no longer positive:
 		if abort,
@@ -37,15 +29,12 @@ Cook food:
 			increase power to heating element
 		if temperature is too high,
 			decrease power to heating element
-
 When instructions/confirmation received:
 	Are instructions valid?
 	 N: report
 	 Y: Note that valid instructions received
-
 When food inserted:
 	Note that food inserted
-
 If food inserted and valid instructions received,
 	lower slot mechanism
 		wait for mechanism to complete
@@ -98,58 +87,61 @@ class CookTimer(object):
 class CookInstruction(object):
 	''' Manage access to cooking instruction, and carry it out '''
 	
-	cook_time = 0
-	cook_temp = 0
-	mutexes = None
+	cook_time, cook_temp = 0,0
+	time_mutex, temp_mutex = None,None
 	timer = None
-	confirmed = None
-	stop = None
+	confirmed, stop = None, None
 
-	def __init__(self, cook_time=0, cook_temp=0)
+	def __init__(self, cook_time=0, cook_temp=0):
 		self.timer = CookTimer()
 		self.cook_time = cook_time
 		self.cook_temp = cook_temp
 		self.stop = thread.Event()
 		self.confirmed = thread.Event() # what is the initial state of a threading.Event object?
-		self.mutexes = [thread.Lock(), thread.Lock()] # might want to split so no list is created
+		self.mutex_time = thread.Lock()
+		self.mutex_temp = thread.Lock()
 	
-	def write_time(value):
+	def write_time(self, value):
+		''' Write access for cooking time '''
 		if value < 0:
 			''' Report time error '''
+			...
 			return
-		with mutexes[0]:
+		with self.mutex_time:
 			self.cook_time = value
 	
-	def write_temperature(value):
+	def write_temperature(self, value):
+		''' Write access for cooking temperature '''
 		if value < 0:
 			''' Report temperature error '''
+			...
 			return
-		with mutexes[1]:
+		with self.mutex_temp:
 			self.cook_temp = value
 	
-	def read_time():
-		with mutexes[0]:
+	def read_time(self):
+		with self.mutex_time:
 			return self.cook_time
 	
-	def read_temperature():
-		with mutexes[1]:
+	def read_temperature(self):
+		with self.mutex_temp:
 			return self.cook_temp
 	
-	def confirm():
+	def confirm(self):
 		self.confirmed.set()
 	
-	def halt():
+	def halt(self):
 		self.stop.set()
 		self.confirmed.clear()
 	
-	def clear():
-		write_time(0)
-		write_temperature(0)
+	def clear(self):
+		self.write_time(0)
+		self.write_temperature(0)
 		self.confirmed.clear()
 		self.stop.clear()
 		self.timer.clear()
 
-	def execute():
+	def execute(self):
 		# lower platform
 		# set stuff about temperature
 		self.timer.set(self.read_time)
@@ -160,6 +152,7 @@ class CookInstruction(object):
 			# check temperature
 			# 	alter something
 			...
+		# raise platform
 		self.clear()
 
 
@@ -170,15 +163,14 @@ def listen(instructions):
 	''' For the time being, let's say this is where it gets messages.
 		For testing purposes, I'll substitute with input() '''
 	while True:
-		time = input('Time (sec): ')
-		temperature = input('Temperature (ºF): ')
-		slot = int(input('Slot? 0/1'))
+		time = int(input('Time (sec)\n> '))
+		temperature = int(input('Temperature (°F)\n> '))
+		slot = int(input('Slot? 0/1\n> '))
 		assert slot == 0 or slot == 1, 'Slot doesn\'t exist'
 		instructions[slot].write_time(time)
 		instructions[slot].write_temperature(temperature)
 		if input('Confirm? > '):
 			instructions[slot].confirm()
-
 
 def execute(instruction):
 	''' this thread carries out instructions received from the app '''
@@ -190,17 +182,12 @@ if __name__ == '__main__':
 	# one instruction structure for each toaster slot
 	instructions = [CookInstruction(), CookInstruction()]
 	# this thread will listen for messages from the app
-	listener = thread.Thread(target=listen, name='listener', args=instructions, daemon=True)
+	listener = thread.Thread(target=listen, name='listener', args=(instructions,), daemon=True)
 	listener.start()
-	execute_0 = thread.Thread(target=execute, name='executor_0', args=instruction[0])
-	execute_1 = thread.Thread(target=execute, name='executor_1', args=instruction[1])
+	execute_0 = thread.Thread(target=execute, name='executor_0', args=(instructions[0],))
+	execute_1 = thread.Thread(target=execute, name='executor_1', args=(instructions[1],))
 	execute_0.start()
 	execute_1.start()
 	listener.join()
 	execute_0.join()
 	execute_1.join()
-
-
-
-
-
