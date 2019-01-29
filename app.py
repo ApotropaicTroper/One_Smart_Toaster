c582ae4a94baab2180c818fcc39d50d0ac5c1472
ryan.sperl <sperl.4@wright.edu>
2019-01-29 09:05:15 -0500

    grid layout

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 kivy.require('1.10.1')
@@ -11,11 +12,18 @@ class AppBase(App):
 		''' Initialize root widget '''
 		return MainMenu()
 
-class MainMenu(GridLayout):
 
-	def __init__(self):
-		super(MainMenu,self).__init__()
+# layout = BoxLayout(orientation='vertical')
+
+class MainMenu(GridLayout):
+	def __init__(self, **kwargs):
+		super(MainMenu,self).__init__(**kwargs)
+		self.rows = 10
 		self.add_widget(Label(text='Hello!'))
+		self.add_widget(Label(text='Welcome'))
+		self.add_widget(Label(text='to'))
+		self.add_widget(Label(text='Smart'))
+		self.add_widget(Label(text='Toaster'))
 		...
 
 
