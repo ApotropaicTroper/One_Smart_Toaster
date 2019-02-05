16b7f3549dd47e8461f8c2ae37d010d4e3ba2a8d
ryan.sperl <sperl.4@wright.edu>
2019-02-05 09:08:52 -0500

    Multiple menus

--- app.py
+++ app.py
@@ -1,35 +1,74 @@
 
 import kivy
 from kivy.app import App
+from kivy.utils import escape_markup
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.button import Button
+# from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+
 kivy.require('1.10.1')
+
+'''
+							Start Menu
+							Main Menu
+	Instruction Entry Menu				Database Menu
+Input Menu		Defaults Menu
+
+'''
+
+class StartMenu(Screen):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.add_widget(Label(text='[i]Touch to begin[/i]', markup=True))
+
+	# overridden method
+	def on_touch_down(self, touch):
+		sm.transition.direction = 'up'
+		sm.current = 'Main'
+
+
+class MainMenu(Screen):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.add_widget(Label(text='[b]Main Menu[/b]', markup=True))
+	def on_touch_down(self, touch):
+		sm.transition.direction = 'up'
+		sm.current = 'Instruction'
+
+
+class InstructionEntryMenu(Screen):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.layout = GridLayout(rows=5, cols=1)
+		self.add_widget(self.layout)
+		self.layout.add_widget(Label(text='test_layout_1'))
+		self.layout.add_widget(Label(text='test_layout_2'))
+		self.time_input = TextInput(text='time_input', multiline=False)
+		self.layout.add_widget(self.time_input)
+
+	
+	# @staticmethod
+	# def on_enter(instance,value):
+	# 	print('testing on_enter')
+
+	# def on_touch_down(self, touch):
+	# 	print(touch.pos)
+	# 	sm.transition.direction = 'down'
+	# 	# sm.current = 'Main'
+
+sm = ScreenManager()	# direction can be left/right/up/down
+# sm.add_widget(StartMenu(name='Start'))
+# sm.add_widget(MainMenu(name='Main'))
+sm.add_widget(InstructionEntryMenu(name='Instruction'))
 
 class AppBase(App):
 	def build(self):
-		''' Initialize root widget '''
-		return MainMenu()
+		return sm
 
-
-
-class MainMenu(GridLayout):
-	...
-
-
-class StartMenu(GridLayout):
-	def __init__(self, **kwargs):
-		super(MainMenu,self).__init__(**kwargs)
-		self.rows = 10
-		self.add_widget(Label(text='Hello!'))
-		self.add_widget(Label(text='Welcome to Smart Toaster'))
-		# self.add_widget(Label(text='to'))
-		# self.add_widget(Label(text='Smart'))
-		# self.add_widget(Label(text='Toaster'))
-		self.add_widget(Button(text='\n\nTouch to begin'))
-
-
-AppBase().run()
+if __name__ == '__main__':
+	AppBase().run()
 
 
 
