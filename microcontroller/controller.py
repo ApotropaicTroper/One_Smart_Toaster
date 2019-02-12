dd63c11909b0a2c3f2ff09b815e34306c0a703b1
ryan.sperl <sperl.4@wright.edu>
2019-02-12 08:23:21 -0500

    moved execute method to main.

--- microcontroller/controller.py
+++ microcontroller/controller.py
@@ -25,14 +25,6 @@ class ToasterController(object):
 		self.instructions.clear()
 		self.raise_platform()
 
-	def execute(self):
-		''' Loop until shut off '''
-		while True:
-			self.instructions.confirmed.wait() # have the instructions been confirmed? wait until this is true
-			self.start()
-			self.instructions.stopped.wait(timeout=self.timer.cook_time) # wait until user signals to stop, up to the length of the timer
-			self.end()
-
 	def lower_platform(self):
 		self.loaded = True
 		...
