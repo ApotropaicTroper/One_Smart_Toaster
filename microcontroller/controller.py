6f88a8c033978413b5779f7b6bf3081004204c62
ryan.sperl <sperl.4@wright.edu>
2019-03-14 07:57:00 -0400

    rudimentary GPIO

--- microcontroller/controller.py
+++ microcontroller/controller.py
@@ -1,5 +1,8 @@
 
 from timer import CookTimer
+from RPi import GPIO
+
+# total of 40 I/O pins
 
 class ToasterController(object):
 
@@ -7,9 +10,39 @@ class ToasterController(object):
 	instructions = None
 	timer = None
 
+
+	heating_element_channel = 0
+	fan_channel = 1
+	platform_channel = 2
+
+	# Requires 3 pins
+	''' Temperature channels '''
+	'''
+	To read temperature, should probably use SPI
+		MISO to DO
+		CS0 to CS
+		SCLK to CLK
+https://cdn-learn.adafruit.com/downloads/pdf/max31855-thermocouple-python-library.pdf
+
+
+	temp_CLK_channel = 28 	# output; clock
+	temp_DO_channel = 29	# input; data out
+	temp_CS_channel = 30	# output; chip select (time to read)
+	'''
+
 	def __init__(self, instructions):
 		self.instructions = instructions
 		self.timer = CookTimer()
+
+		GPIO.setup(self.heating_element_channel, GPIO.OUT)
+		GPIO.setup(self.fan_channel, GPIO.OUT)
+		GPIO.setup(self.platform_channel, GPIO.OUT)
+
+		GPIO.setup(self.temp_CLK_channel, GPIO.OUT)
+		GPIO.setup(self.temp_DO_channel, GPIO.IN)
+		GPIO.setup(self.temp_CS_channel, GPIO.OUT)
+
+
 
 	def start(self):
 		''' Begin cooking '''
@@ -34,4 +67,11 @@ class ToasterController(object):
 	def set_heating_element(self):
 		...
 	def get_temperature(self):
+		GPIO.output(self.temp_CS_channel, GPIO.HIGH)
+
+
+
+
+
+		GPIO.output(self.temp_CS_channel, GPIO.LOW)
 		...
--- microcontroller/main.py
+++ microcontroller/main.py
@@ -1,6 +1,6 @@
 import socket
 import threading as thread
-
+from RPi import GPIO
 
 from controller import ToasterController
 from instruction import CookInstructions
@@ -14,79 +14,86 @@ print(host)
 s.bind((host, port))  # Bind to the port
 
 try:
-    s.listen(1)  # Now wait for client connection.
+	s.listen(1)  # Now wait for client connection.
 except timeout: 
-    print("Server Timed out...closing socket")
-    s.close()
-    
+	print("Server Timed out...closing socket")
+	s.close()
+	
 while True:
-    try:
-        c, addr = s.accept()  # Establish connection with client.
-        print("Server Was Setup!")
-        print('Got connection from', addr)
-        output = 'Thank you for connecting'
-        c.sendall(output.encode('utf-8'))
-        data = c.recv(12345).decode()        #data is accepted in a byte stream
-        all_data = data.split(' ', 1)    #split the data coming in into an array (0 is cook time, 1 is cook temp)
-        break;
-    except socket.error:
-        print("An error has occurred... closing connection to client")
-        print("...Closing socket")
-        c.close()
-    #finally:
-    #c.shutdown(socket.SHUT_RDWR)
-        
+	try:
+		c, addr = s.accept()  # Establish connection with client.
+		print("Server Was Setup!")
+		print('Got connection from', addr)
+		output = 'Thank you for connecting'
+		c.sendall(output.encode('utf-8'))
+		data = c.recv(12345).decode()	 #data is accepted in a byte stream
+		all_data = data.split(' ', 1)   #split the data coming in into an array (0 is cook time, 1 is cook temp)
+		break;
+	except socket.error:
+		print("An error has occurred... closing connection to client")
+		print("...Closing socket")
+		c.close()
+	#finally:
+	#c.shutdown(socket.SHUT_RDWR)
+		
 
 #def recieve_data(instructions):
-#    try:
-#        #Recieve the cook time and temp as an array so it can be transferred as one parameter then split it up        
-#        print('Cook Time:', all_data [0])
-#        print('Cook Temp:', all_data [1])
-#        time_num = int(all_data [0])
-#        #instructions = listen(instructions)
-#        temp_num = int(all_data [1])
-#    except socket.error:
-#        print("An error has occured...")
-
+#   try:
+#	 #Recieve the cook time and temp as an array so it can be transferred as one parameter then split it up  
+#	 print('Cook Time:', all_data [0])
+#	 print('Cook Temp:', all_data [1])
+#	 time_num = int(all_data [0])
+#	 #instructions = listen(instructions)
+#	 temp_num = int(all_data [1])
+#   except socket.error:
+#	 print("An error has occured...")
 def listen(instructions):
-    '''this thread listens for messages from the app'''
-    try:
-        #Recieve the cook time and temp as an array so it can be transferred as one parameter then split it up        
-        print('Cook Time:', int(all_data[0]))
-        print('Cook Temp:', int(all_data[1]))
-        
-        #time_num = int(all_data [0])
-        #instructions = listen(instructions)
-        #temp_num = int(all_data [1])
-    except socket.error:
-        print("An error has occured...")
-#    while True:
-#        input('>')
-#        '''Upon recieving confirmation, call instructions.confirm()
-#           Upon recieving stop order, call instructions.stop()
-#        '''
+	'''this thread listens for messages from the app'''
+	try:
+		#Recieve the cook time and temp as an array so it can be transferred as one parameter then split it up  
+		print('Cook Time:', int(all_data[0]))
+		print('Cook Temp:', int(all_data[1]))
+		
+		#time_num = int(all_data [0])
+		#instructions = listen(instructions)
+		#temp_num = int(all_data [1])
+	except socket.error:
+		print("An error has occured...")
+#   while True:
+#	 input('>')
+#	 '''Upon recieving confirmation, call instructions.confirm()
+#	   Upon recieving stop order, call instructions.stop()
+#	 '''
 
 def execute(controller, instructions):
-    ''' Loop until shut off '''
-    while True:
-        instructions.confirmed.wait() # have the instructions been confirmed? wait until this is true
-        controller.start()
-        instructions.stopped.wait(timeout=instructions.cook_time) # wait until user signals to stop, up to the length of the timer
-        controller.end()
+	GPIO.setmode(GPIO.BOARD)
+
+
+	try:
+		''' Loop until shut off '''
+		while True:
+			instructions.confirmed.wait() # have the instructions been confirmed? wait until this is true
+			controller.start()
+			instructions.stopped.wait(timeout=instructions.cook_time) # wait until user signals to stop, up to the length of the timer
+			controller.end()
+
+	finally:
+		GPIO.cleanup()
 
 
 ''' For future reference, using end='\r' allows printing over same line '''
 if __name__ == '__main__':
 # one instruction structure for each toaster slot
-    
-    instructions = CookInstructions()
-    listen(instructions);
-    controller = ToasterController(instructions)
-# this thread will listen for messages from the app
-    listener = thread.Thread(target=listen, name='listener', args=(instructions,), daemon=True)
-    listener.start()
-    execute = thread.Thread(target=execute, name='executor', args=(controller, instructions))
-    execute.start()
-    listener.join()
-    execute.join()
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.getmode()
+	# instructions = CookInstructions()
+	# controller = ToasterController(instructions)
+# this thread will listen for messages from the app (also send messages back?)
+	# listener = thread.Thread(target=listen, name='listener', args=(instructions,), daemon=True)
+	# listener.start()
+# this thread will control the toaster hardware
+	# execute = thread.Thread(target=execute, name='executor', args=(controller, instructions))
+	# execute.start()
+	# listener.join()
+	# execute.join()
 
