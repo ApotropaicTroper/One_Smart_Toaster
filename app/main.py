fe35affc3ea20f98b69169a9be2ce46cd4217573
ApotropaicTroper <ApotropaicTroper@users.noreply.github.com>
2019-03-19 09:10:11 -0400

    android's kivy launcher crashes when run

--- app/main.py
+++ app/main.py
@@ -10,12 +10,10 @@ from main_menu import MainMenu
 
 class AppBase(App):
 	def build(self):
+		root = RootMenu()
+		# each Menu creates/destroys its children
+		root.add_child(MainMenu(name='Main'))
 		return root
 
 if __name__ == '__main__':
-	root = RootMenu()
-	# each Menu creates/destroys its children
-	root.add_child(MainMenu(name='Main'))
-
-
 	AppBase().run()
