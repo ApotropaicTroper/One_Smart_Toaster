704a39e8b83051203e26e64e1a41e7458d4db400
mayoWill <47146587+mayoWill@users.noreply.github.com>
2019-04-25 01:00:27 -0400

    Update manager.py

--- app/manager.py
+++ app/manager.py
@@ -2,10 +2,11 @@ import kivy
 from kivy.utils import escape_markup
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.label import Label
-from kivy.core.window import Window
 from kivy.clock import Clock
 
 import socket
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
 
 
 
@@ -15,7 +16,7 @@ class Menu(Screen):
 
 	# static variable; all Menu objects access the same ScreenManager
 	_sm = ScreenManager()
-	size = Window.size
+	size = (800,600)
 
 	# static, read-only variable
 	@property
@@ -45,32 +46,20 @@ class Menu(Screen):
 		self.switch_to_parent()
 
 	def send(self, s, message):
-		''' Send message string to pi 
-		Error codes:
-			0: no error
-			1: socket error
-			2: OSError
-		'''
+		''' Send message string to pi '''
 		try:
 			s.send(message.encode('utf-8'))
 		except socket.error:
 			print("An error has occurred... couldn't send data")
-			return 1
-		except OSError:
-			print("An error has occurred... couldn't send data")
-			return 2
-		else:
-			return 0
-
 
 	def recv(self, c):
 		''' Receive data from pi (such as remaining time or current temperature '''
+		data = 0
 		try:
 			data = c.recv(12345).decode()
-		except OSError:
-			print("An error has occurred... couldn't receive data")
-		else:
-			return data
+		except socket.error:
+			print("An error has occurred... couldn't send data")
+		return data
 
 	def to_minsec(self, seconds):
 		''' format seconds as minutes:seconds '''
@@ -80,6 +69,7 @@ class Menu(Screen):
 	def to_sec(self, minsec):
 		''' format minutes:seconds as seconds '''
 		*minutes, seconds = minsec.split(':')
+
 		if not minutes:
 			return int(seconds)
 		return int(minutes[0])*60 + int(seconds)
@@ -101,6 +91,8 @@ class RootMenu(Menu):
 
 	def on_touch_down(self, touch):
 		self.switch_to_child('Main')
+
+
 
 
 
